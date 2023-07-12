587a6c03915cd95bcaf2fcedfa846a8f9c6859ff
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-13 00:33:19 +0800

    Add DbContext

---
+++ api/User Contact Management System/Data/APIDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using User_Contact_Management_System.Models;
+
+namespace User_Contact_Management_System.Data
+{
+    public class APIDbContext : DbContext
+    {
+        private readonly string? _connectionString;
+
+        public APIDbContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SqlConnection");
+        }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
