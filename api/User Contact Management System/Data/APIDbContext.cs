f414e7408efacabeafc4c1ecda34df15ef1169e1
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-14 10:40:40 +0800

    Update APIDbContext to hide connection string

--- api/User Contact Management System/Data/APIDbContext.cs
+++ api/User Contact Management System/Data/APIDbContext.cs
@@ -5,15 +5,8 @@ namespace User_Contact_Management_System.Data
 {
     public class APIDbContext : DbContext
     {
-        private readonly string? _connectionString;
-
-        public APIDbContext(IConfiguration configuration)
-        {
-            _connectionString = configuration.GetConnectionString("SqlConnection");
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(_connectionString);
-        }
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
