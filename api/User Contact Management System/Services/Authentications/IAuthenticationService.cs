79f2ca4a9e44af67b3bbd101cda966deb4d7a4eb
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-15 13:09:06 +0800

    Add Authentication service

---
+++ api/User Contact Management System/Services/Authentications/IAuthenticationService.cs
@@ -0,0 +1,7 @@
+﻿namespace User_Contact_Management_System.Services.Authentications
+{
+    public interface IAuthenticationService
+    {
+        Task<string?> GenerateJwtToken(string username);
+    }
+}
