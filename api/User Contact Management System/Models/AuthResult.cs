63fd315cc50faa3459bfa5d96a82d10748bd3590
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-15 18:29:14 +0800

    Update models

---
+++ api/User Contact Management System/Models/AuthResult.cs
@@ -0,0 +1,9 @@
+﻿namespace User_Contact_Management_System.Models
+{
+    public class AuthResult
+    {
+        public string? Token { get; set; }
+        public bool Result { get; set; }
+        public string? Error { get; set; }
+    }
+}
