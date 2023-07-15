9a4dc4212644581e30c755524dd08c2c84adda69
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-15 13:08:52 +0800

    Add User service

---
+++ api/User Contact Management System/Services/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using User_Contact_Management_System.Dtos.Users;
+using User_Contact_Management_System.Models;
+
+namespace User_Contact_Management_System.Services.Users
+{
+    public interface IUserService
+    {
+        Task<User?> CreateUser(UserCreateDto user);
+        Task<User?> GetUserByUsername(string username);
+        Task<User?> GetUserByEmail(string email);
+        Task<bool> UpdateUserByUsername(string username);
+        Task<bool> DeleteUser(string username);
+    }
+}
---
+++ api/User Contact Management System/Services/Users/UserService.cs
@@ -0,0 +1,50 @@
+﻿using User_Contact_Management_System.Dtos.Users;
+using User_Contact_Management_System.Models;
+using User_Contact_Management_System.Repositories.Users;
+
+namespace User_Contact_Management_System.Services.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User?> CreateUser(UserCreateDto user)
+        {
+            var newuser = new User { 
+                FirstName = user.FirstName,
+                Email = user.Email, 
+                LastName = user.Email,  
+                Username = user.Username,
+            };
+
+            await _userRepository.CreateUser(newuser);
+
+            return newuser;
+        }
+
+        public Task<bool> DeleteUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateUserByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
