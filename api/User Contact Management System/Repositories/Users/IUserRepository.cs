4e1d9c79e3d53f692c699299f1a7b9154beb7b20
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-15 13:08:43 +0800

    Add User Repository

---
+++ api/User Contact Management System/Repositories/Users/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using User_Contact_Management_System.Models;
+
+namespace User_Contact_Management_System.Repositories.Users
+{
+    public interface IUserRepository
+    {
+        Task<int> CreateUser(User user);
+        Task<User?> GetUserByUsername(string username);
+        Task<bool> UpdateUser(User user);
+        Task<bool> DeleteUser(int id);
+    }
+}
---
+++ api/User Contact Management System/Repositories/Users/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using User_Contact_Management_System.Data;
+using User_Contact_Management_System.Models;
+
+namespace User_Contact_Management_System.Repositories.Users
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly APIDbContext _context;
+
+        public UserRepository(APIDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CreateUser(User user)
+        {
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    await _context.Users.AddAsync(user);
+                    await _context.SaveChangesAsync();
+
+                    transaction.Commit();
+                    return user.Id;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+        public async Task<User?> GetUserByUsername(string username)
+        {
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);        
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+        public Task<bool> DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
