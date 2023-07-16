956bbdb2b86ce98719724f5322e8a028e43e6365
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-16 14:59:22 +0800

    Add Update methods in UsersController

--- api/User Contact Management System/Controllers/UsersController.cs
+++ api/User Contact Management System/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using User_Contact_Management_System.Dtos.Users;
 using User_Contact_Management_System.Models;
 using User_Contact_Management_System.Services.Users;
@@ -18,41 +20,109 @@ namespace User_Contact_Management_System.Controllers
             _userService = userService;
         }
 
-        [HttpPost("SignUp", Name = "CreateUser")]
+        private IActionResult GetBadRequest(string errorMessage)
+        {
+            return BadRequest(new AuthResult()
+            {
+                Result = false,
+                Token = null,
+                Error = errorMessage
+            });
+        }
+
+        [HttpPost("Register")]
         [Consumes("application/json")]
         [Produces("application/json")]
-        public async Task<IActionResult> CreateUser(UserCreateDto user)
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(UserCreateDto user)
         {
             try
             {
-                if (ModelState.IsValid)
-                {
-                    var authResult = await _userService.CreateUser(user);
+                if (!ModelState.IsValid)
+                    return GetBadRequest("Invalid request.");
 
-                    if (authResult == null)
-                    {
-                        return BadRequest(new AuthResult()
-                        {
-                            Result = false,
-                            Token = null,
-                            Error = "Username or Email already exists."
-                        });
-                    }
+                var authResult = await _userService.Register(user);
 
-                    return Ok(authResult);
-                }
-           
-                return BadRequest(new AuthResult()
-                {
-                    Result = false,
-                    Token = null,
-                    Error = "Invalid request."
-                });
+                return authResult != null ? Ok(authResult) : GetBadRequest("Username or Email already exists.");
             }
             catch (Exception e)
             {
-                _logger.LogError(e.Message);
-                return StatusCode(500, "An error occurred while creating the User.");
+                _logger.LogError(e, $"{nameof(_userService.Register)} threw an exception");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPost("Login")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserLoginDto user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return GetBadRequest("Invalid request.");
+
+                var authResult = await _userService.Login(user);
+
+                return authResult != null ? Ok(authResult) : GetBadRequest("Invalid credentials.");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"{nameof(_userService.Login)} threw an exception");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPut("UpdateDetails")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [Authorize]
+        public async Task<IActionResult> UpdateUser(UserUpdateDetailsDto user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return GetBadRequest("Invalid request.");
+
+                var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (userId == null)
+                    return GetBadRequest("Invalid user.");
+
+                var result = await _userService.UpdateUserDetails(userId, user);
+
+                return result ? Ok(result) : GetBadRequest("Invalid parameters.");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"{nameof(_userService.UpdateUserDetails)} threw an exception");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPut("UpdatePassword")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [Authorize]
+        public async Task<IActionResult> UpdatePassword(UserUpdatePasswordDto user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return GetBadRequest("Invalid request.");
+
+                var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (userId == null)
+                    return GetBadRequest("Invalid user.");
+
+                var result = await _userService.UpdateUserPassword(userId, user);
+
+                return result ? Ok(result) : GetBadRequest("Invalid parameters.");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"{nameof(_userService.UpdateUserPassword)} threw an exception");
+                return StatusCode(500, "Internal server error");
             }
         }
     }
