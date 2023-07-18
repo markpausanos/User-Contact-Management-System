deea8fdbeee6392d1bbd1cb6103181db76a3fb23
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-18 16:55:37 +0800

    Add Contact Repository

---
+++ api/UCMS-api/Repositories/Contacts/IContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using User_Contact_Management_System.Models;
+
+namespace User_Contact_Management_System.Repositories.Contacts
+{
+    public interface IContactRepository
+    {
+        Task<Contact?> CreateContact(Contact contact);
+        Task<IEnumerable<Contact>> GetAllContacts(string userId);
+        Task<Contact?> GetContact(string userId, string id);
+        Task<bool> UpdateContact(Contact contact);
+        Task<bool> DeleteContact(Contact contact);
+    }
+}
