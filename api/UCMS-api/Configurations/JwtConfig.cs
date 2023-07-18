a22c3cbb16f2a28189eb0748032766a36b67e5e7
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-18 16:55:55 +0800

    Update jwt config

--- api/UCMS-api/Configurations/JwtConfig.cs
+++ api/UCMS-api/Configurations/JwtConfig.cs
@@ -5,6 +5,6 @@
         public string? Secret { get; set; }
         public string? Issuer { get; set; }
         public string? Audience { get; set; }
-        public int ExpirationMinutes { get; set; }
+        public TimeSpan ExpirationTimeFrame { get; set; }
     }
 }
