13b98dcf8d44ffe84298f86858cf941c29b7e15e
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-18 22:16:53 +0800

    Update UserServiceTests

--- api/UCMS-test/UserServiceTests.cs
+++ api/UCMS-test/UserServiceTests.cs
@@ -4,20 +4,49 @@ using User_Contact_Management_System.Dtos.Users;
 using User_Contact_Management_System.Repositories.Users;
 using User_Contact_Management_System.Models;
 using User_Contact_Management_System.Configurations;
+using User_Contact_Management_System.Repositories.RefreshTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using AutoMapper;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace UCMS_test
 {
     public class UserServiceTest
     {
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<IRefreshTokenRepository> _mockRefreshTokenRepository;
         private readonly Mock<IUserRepository> _mockUserRepository;
+        private readonly Mock<JwtSecurityTokenHandler> _mockJwtSecurityTokenHandler;
         private readonly UserService _userService;
         private readonly JwtConfig _jwtConfig;
+        private readonly TokenValidationParameters _tokenValidationParameters;
 
         public UserServiceTest()
         {
+            _mockMapper = new Mock<IMapper>();
             _mockUserRepository = new Mock<IUserRepository>();
-            _jwtConfig = new JwtConfig { Secret = "This is a very secret key" };
-            _userService = new UserService(_mockUserRepository.Object, _jwtConfig);
+            _mockRefreshTokenRepository = new Mock<IRefreshTokenRepository>();
+            _mockJwtSecurityTokenHandler = new Mock<JwtSecurityTokenHandler>();
+            _jwtConfig = new JwtConfig
+            {
+                Secret = "This is a very secret key",
+                Audience = "Audience",
+                Issuer = "Issuer",
+                ExpirationTimeFrame = new TimeSpan(0, 1, 0)
+            };
+            _tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                RequireExpirationTime = false,
+                ValidIssuer = _jwtConfig.Issuer,
+                ValidAudience = _jwtConfig.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.Secret!))
+            };
+            _userService = new UserService(_mockMapper.Object, _mockUserRepository.Object, _mockRefreshTokenRepository.Object, _jwtConfig, _tokenValidationParameters);
         }
 
         [Fact]
@@ -32,16 +61,29 @@ namespace UCMS_test
                 FirstName = "Test",
                 LastName = "User"
             };
-            ApplicationUser appUser = null;
+
+            var applicationUser = new ApplicationUser
+            {
+                Id = "NewId",
+                Email = "Newemail@email.com"
+            };
+
+            var refreshToken = new RefreshToken
+            {
+                Token = "RefreshToken"
+            };
 
             _mockUserRepository.Setup(x => x.GetUserByUsername(It.IsAny<string>()))
-                .ReturnsAsync(() => appUser);
+                .ReturnsAsync(() => null);
 
             _mockUserRepository.Setup(x => x.GetUserByEmail(It.IsAny<string>()))
-                .ReturnsAsync(() => appUser);
+                .ReturnsAsync(() => null);
 
             _mockUserRepository.Setup(x => x.CreateUser(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
-                .ReturnsAsync(() => "NewUserID");
+                .ReturnsAsync(() => applicationUser);
+
+            _mockRefreshTokenRepository.Setup(x => x.CreateRefreshToken(It.IsAny<RefreshToken>()))
+                .ReturnsAsync(() => refreshToken);
 
             // Act
             var result = await _userService.Register(userCreateDto);
@@ -50,78 +92,7 @@ namespace UCMS_test
             Assert.NotNull(result);
             Assert.True(result.Result);
             Assert.NotNull(result.Token);
-        }
-
-        [Fact]
-        public async Task Login_WithValidData_ReturnsAuthResult()
-        {
-            // Arrange
-            var userLoginDto = new UserLoginDto
-            {
-                Username = "testuser",
-                Password = "Test1234"
-            };
-
-            var appUser = new ApplicationUser
-            {
-                Id = "TestUserId",
-                Email = "testuser@example.com",
-                UserName = userLoginDto.Username
-            };
-
-            _mockUserRepository.Setup(x => x.GetUserByUsername(It.IsAny<string>()))
-                .ReturnsAsync(() => appUser);
-
-            _mockUserRepository.Setup(x => x.CheckPasswordIsValid(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
-                .ReturnsAsync(() => true);
-
-            // Act
-            var result = await _userService.Login(userLoginDto);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.True(result.Result);
-            Assert.NotNull(result.Token);
-        }
-
-        [Fact]
-        public async Task UpdateUserDetails_WithValidData_ReturnsTrue()
-        {
-            // Arrange
-            var userUpdateDetailsDto = new UserUpdateDetailsDto
-            {
-                FirstName = "UpdatedFirstName",
-                LastName = "UpdatedLastName"
-            };
-
-            _mockUserRepository.Setup(x => x.UpdateUserDetails(It.IsAny<ApplicationUser>()))
-                .ReturnsAsync(() => true);
-
-            // Act
-            var result = await _userService.UpdateUserDetails("SomeUserID", userUpdateDetailsDto);
-
-            // Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public async Task UpdateUserPassword_WithValidData_ReturnsTrue()
-        {
-            // Arrange
-            var userUpdatePasswordDto = new UserUpdatePasswordDto
-            {
-                OldPassword = "OldPassword",
-                NewPassword = "NewPassword"
-            };
-
-            _mockUserRepository.Setup(x => x.UpdateUserPassword(It.IsAny<ApplicationUser>(), It.IsAny<string>(), It.IsAny<string>()))
-                .ReturnsAsync(() => true);
-
-            // Act
-            var result = await _userService.UpdateUserPassword("SomeUserID", userUpdatePasswordDto);
-
-            // Assert
-            Assert.True(result);
+            Assert.NotNull(result.RefreshToken);
         }
 
         [Fact]
@@ -136,6 +107,7 @@ namespace UCMS_test
                 FirstName = "Test",
                 LastName = "User"
             };
+
             var existingUser = new ApplicationUser
             {
                 UserName = "testuser"
@@ -163,6 +135,7 @@ namespace UCMS_test
                 FirstName = "Test",
                 LastName = "User"
             };
+
             var existingUser = new ApplicationUser
             {
                 Email = "testuser@example.com"
@@ -179,6 +152,47 @@ namespace UCMS_test
         }
 
         [Fact]
+        public async Task Login_WithValidData_ReturnsAuthResult()
+        {
+            // Arrange
+            var userLoginDto = new UserLoginDto
+            {
+                Username = "testuser",
+                Password = "Test1234"
+            };
+
+            var applicationUser = new ApplicationUser
+            {
+                Id = "TestUserId",
+                Email = "testuser@example.com",
+                UserName = userLoginDto.Username
+            };
+
+            var refreshToken = new RefreshToken
+            {
+                Token = "RefreshToken"
+            };
+
+            _mockUserRepository.Setup(x => x.GetUserByUsername(It.IsAny<string>()))
+                .ReturnsAsync(() => applicationUser);
+
+            _mockUserRepository.Setup(x => x.CheckPasswordIsValid(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+                .ReturnsAsync(() => true);
+
+            _mockRefreshTokenRepository.Setup(x => x.CreateRefreshToken(It.IsAny<RefreshToken>()))
+                .ReturnsAsync(() => refreshToken);
+
+            // Act
+            var result = await _userService.Login(userLoginDto);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.Result);
+            Assert.NotNull(result.Token);
+            Assert.NotNull(result.RefreshToken);
+        }
+
+        [Fact]
         public async Task Login_WithInvalidPassword_ReturnsNull()
         {
             // Arrange
@@ -188,13 +202,13 @@ namespace UCMS_test
                 Password = "WrongPassword"
             };
 
-            var appUser = new ApplicationUser
+            var applicationUser = new ApplicationUser
             {
                 UserName = userLoginDto.Username
             };
 
             _mockUserRepository.Setup(x => x.GetUserByUsername(It.IsAny<string>()))
-                .ReturnsAsync(() => appUser);
+                .ReturnsAsync(() => applicationUser);
 
             _mockUserRepository.Setup(x => x.CheckPasswordIsValid(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
                 .ReturnsAsync(() => false);
@@ -216,16 +230,70 @@ namespace UCMS_test
                 Password = "Test1234"
             };
 
-            ApplicationUser appUser = null;
-
             _mockUserRepository.Setup(x => x.GetUserByUsername(It.IsAny<string>()))
-                .ReturnsAsync(() => appUser);
+                .ReturnsAsync(() => null);
 
             // Act
             var result = await _userService.Login(userLoginDto);
 
             // Assert
             Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task UpdateUserDetails_WithValidData_ReturnsTrue()
+        {
+            // Arrange
+            var userUpdateDetailsDto = new UserUpdateDetailsDto
+            {
+                FirstName = "UpdatedFirstName",
+                LastName = "UpdatedLastName"
+            };
+
+            var applicationUser = new ApplicationUser
+            {
+                Id = "TestUserId"
+            };
+
+            _mockUserRepository.Setup(x => x.GetUserById(It.IsAny<string>()))
+                .ReturnsAsync(() => applicationUser);
+
+            _mockUserRepository.Setup(x => x.UpdateUserDetails(It.IsAny<ApplicationUser>()))
+                .ReturnsAsync(() => true);
+
+            // Act
+            var result = await _userService.UpdateUserDetails("SomeUserID", userUpdateDetailsDto);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task UpdateUserPassword_WithValidData_ReturnsTrue()
+        {
+            // Arrange
+            var userUpdatePasswordDto = new UserUpdatePasswordDto
+            {
+                OldPassword = "OldPassword",
+                NewPassword = "NewPassword"
+            };
+
+            var applicationUser = new ApplicationUser
+            {
+                Id = "TestUserId"
+            };
+
+            _mockUserRepository.Setup(x => x.GetUserById(It.IsAny<string>()))
+                .ReturnsAsync(() => applicationUser);
+
+            _mockUserRepository.Setup(x => x.UpdateUserPassword(It.IsAny<ApplicationUser>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(() => true);
+
+            // Act
+            var result = await _userService.UpdateUserPassword("SomeUserID", userUpdatePasswordDto);
+
+            // Assert
+            Assert.True(result);
         }
 
         [Fact]
@@ -247,6 +315,5 @@ namespace UCMS_test
             // Assert
             Assert.False(result);
         }
-
     }
 }
