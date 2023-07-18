85544a9da88a6ae040d6613ab2d60148be404863
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-18 16:56:47 +0800

    Add GetCurrentUser util

---
+++ api/UCMS-api/Utils/UserUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace User_Contact_Management_System.Utils
+{
+    public class UserUtils
+    {
+        public string? GetCurrentUser(HttpContext context)
+        {
+            var identity = context.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                var userId = userClaims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier)?.Value;
+
+                return userId;
+            }
+
+            return null;
+        }
+    }
+}
