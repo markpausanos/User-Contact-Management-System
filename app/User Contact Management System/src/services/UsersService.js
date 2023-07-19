28d1267202a34039091d25adf859ffe7a0fa9cff
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-19 21:57:02 +0800

    Add services

---
+++ app/User Contact Management System/src/services/UsersService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import config from "./config";
+
+const BASE_URL = `${config.API_URL}/Users`;
+const REFRESH_TOKEN_URL = `${BASE_URL}/Tokens/Refresh`;
+
+const UsersService = {
+	login: (user) => axios.post(BASE_URL, user),
+	register: (user) => axios.post(BASE_URL, user),
+	refreshToken: (userTokenRequest) =>
+		axios.post(REFRESH_TOKEN_URL, userTokenRequest),
+	updatePassword: (userUpdatePassword) =>
+		axios.put(`${BASE_URL}`, userUpdatePassword),
+	updateDetails: (userUpdateDetails) =>
+		axios.put(`${BASE_URL}/Details`, userUpdateDetails),
+};
+
+export default UsersService;
