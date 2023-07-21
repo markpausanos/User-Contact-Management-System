7be3b7238a6946e2e5eb24e94ea56f64db38b5a8
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-22 06:57:33 +0800

    Add Login Screen

---
+++ app/User Contact Management System/src/screens/public/Login/index.jsx
@@ -0,0 +1,222 @@
+import { useState, useContext } from "react";
+import Cookies from "universal-cookie";
+import { Form, Formik } from "formik";
+import { isEmpty } from "lodash";
+import { UserContext } from "../../../contexts";
+import { UsersService } from "../../../services";
+import styles from "./styles.module.scss";
+import {
+	Avatar,
+	Container,
+	Paper,
+	TextField,
+	Button,
+	Typography,
+	FormControlLabel,
+	Checkbox,
+	InputAdornment,
+	IconButton,
+} from "@mui/material";
+import { PulseLoader } from "react-spinners";
+import phone from "../../../statics/images/phone.png";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+const validate = (values) => {
+	const errors = {};
+
+	if (!values.username) {
+		errors.username = "This field is required.";
+	}
+
+	if (!values.password) {
+		errors.password = "This field is required.";
+	}
+
+	return errors;
+};
+
+const Login = () => {
+	const userContext = useContext(UserContext);
+	const cookies = new Cookies();
+	const [isLoggingIn, setIsLoggingIn] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
+
+	return (
+		<div className={styles.Login}>
+			<div className={styles.Login_Header}>
+				<div>
+					<Avatar
+						className={styles.Login_Header_Logo}
+						alt="LinkUp"
+						sx={{ height: "5.5vh", width: "5.5vh" }}
+						src={phone}
+						variant="circular"
+					/>
+				</div>
+				<div>
+					<h2>LinkUp</h2>
+				</div>
+			</div>
+			<Container className={styles.Login_Container}>
+				<Paper elevation={10} className={styles.Login_Container_Paper}>
+					<div className={styles.Login_Container_Paper_Grid}>
+						<h1>Login to LinkUp</h1>
+					</div>
+					<hr style={{ width: "50%" }} />
+
+					<Formik
+						initialValues={{
+							username: "",
+							password: "",
+							remember: false,
+						}}
+						onSubmit={async (values, { setErrors }) => {
+							const currentFormValues = {
+								username: values.username,
+								password: values.password,
+								remember: values.remember,
+							};
+
+							const errors = validate(values);
+
+							if (!isEmpty(errors)) {
+								setErrors(errors);
+								return;
+							}
+
+							setIsLoggingIn(true);
+
+							try {
+								const { data: loginResponse } = await UsersService.login(
+									currentFormValues
+								);
+
+								console.log(loginResponse.token);
+
+								cookies.set("AccessToken", loginResponse.token, {
+									path: "/",
+								});
+
+								if (currentFormValues.remember) {
+									cookies.set("RefreshToken", loginResponse.refreshToken, {
+										path: "/",
+									});
+								}
+								userContext.loginUpdate({
+									username: currentFormValues.username,
+								});
+								setErrors(null);
+							} catch (error) {
+								setErrors({
+									overall: "Invalid username and/or password.",
+								});
+							}
+
+							setIsLoggingIn(false);
+						}}
+					>
+						{({ errors, values, handleSubmit, setFieldValue }) => (
+							<Form className={styles.Login_Container_Paper_Form}>
+								<div className={styles.Login_Container_Paper_Grid}>
+									<TextField
+										label="Username"
+										className={styles.Login_Container_Paper_Textfield}
+										name="username"
+										value={values.username}
+										error={!isEmpty(errors)}
+										helperText={errors.username}
+										required
+										margin="normal"
+										size="small"
+										onChange={(e) => setFieldValue("username", e.target.value)}
+									/>
+									<TextField
+										label="Password"
+										className={styles.Login_Container_Paper_Textfield}
+										name="password"
+										value={values.password}
+										error={!isEmpty(errors)}
+										helperText={errors.password}
+										required
+										margin="normal"
+										type={showPassword ? "text" : "password"}
+										size="small"
+										onChange={(e) => setFieldValue("password", e.target.value)}
+										InputProps={{
+											endAdornment: (
+												<InputAdornment position="end">
+													<IconButton
+														aria-label="toggle password visibility"
+														onClick={() => setShowPassword((show) => !show)}
+														onMouseDown={(e) => e.preventDefault()}
+														edge="end"
+													>
+														{showPassword ? <Visibility /> : <VisibilityOff />}
+													</IconButton>
+												</InputAdornment>
+											),
+										}}
+									/>
+									<FormControlLabel
+										control={
+											<Checkbox
+												name="remember"
+												value={values.remember}
+												onChange={(e) => {
+													setFieldValue("remember", e.target.checked);
+												}}
+											/>
+										}
+										label="Remember me?"
+									/>
+								</div>
+								<div className={styles.Login_Container_Paper_Grid}>
+									<Typography margin="10px" color="red" variant="subtitle2">
+										{errors && errors.overall}
+										{!errors && ""}
+									</Typography>
+								</div>
+								<div className={styles.Login_Container_Paper_Grid}>
+									<Button
+										className={styles.Login_Container_Paper_Button}
+										variant="contained"
+										type="submit"
+										color="primary"
+										disabled={isLoggingIn}
+										onClick={handleSubmit}
+									>
+										{!isLoggingIn && "Sign In"}
+										{isLoggingIn && <PulseLoader color="white" />}
+									</Button>
+								</div>
+							</Form>
+						)}
+					</Formik>
+
+					<div>
+						<Typography
+							className={styles.Login_Container_Paper_Typography}
+							variant="body2"
+							margin={"20px 0px 10px 0px"}
+						>
+							Forgot your password?
+						</Typography>
+					</div>
+					<hr style={{ width: "50%" }} />
+					<div>
+						<Typography
+							className={styles.Login_Container_Paper_Typography}
+							variant="body2"
+							marginTop={"30px"}
+							paddingBottom={"20px"}
+						>
+							Don{`'`}t have an account? Sign Up for LinkUp
+						</Typography>
+					</div>
+				</Paper>
+			</Container>
+		</div>
+	);
+};
+
+export default Login;
