327e7fe1508fc6df1c91cc7ef349c2abf299ec2e
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-22 06:58:24 +0800

    Setup main files

--- app/User Contact Management System/src/App.jsx
+++ app/User Contact Management System/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import { UserContext } from "./contexts";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PrivateRoute, NoAuthRoute } from "./hocs";
+import { PulseLoader } from "react-spinners";
+import { Login, ScreenNoExist } from "./screens/public";
+import "./styles/App.scss";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const cookies = new Cookies();
+const theme = createTheme({
+	typography: {
+		fontFamily: "DIN",
+	},
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [user, setUser] = useState(cookies.get("user"));
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const loginUpdate = (userData) => {
+		cookies.set("user", userData, {
+			path: "/",
+		});
+
+		setUser(userData);
+	};
+
+	const loginRestart = () => {
+		cookies.remove("user", {
+			path: "/",
+		});
+		cookies.remove("accessToken", {
+			path: "/",
+		});
+
+		setUser(null);
+	};
+
+	return (
+		<React.Suspense fallback={<PulseLoader />}>
+			<ThemeProvider theme={theme}>
+				<UserContext.Provider value={{ user, loginUpdate, loginRestart }}>
+					<BrowserRouter>
+						<Routes>
+							<Route
+								exact
+								path="/"
+								element={<PrivateRoute>{<div>Hello world!</div>}</PrivateRoute>}
+							/>
+							<Route
+								path="/home"
+								element={<PrivateRoute element={<div>Hello world!</div>} />}
+							/>
+							<Route
+								path="/login"
+								element={
+									<NoAuthRoute>
+										<Login />
+									</NoAuthRoute>
+								}
+							/>
+							<Route path="*" element={<ScreenNoExist />} />
+						</Routes>
+					</BrowserRouter>
+				</UserContext.Provider>
+			</ThemeProvider>
+		</React.Suspense>
+	);
 }
 
-export default App
+export default App;
