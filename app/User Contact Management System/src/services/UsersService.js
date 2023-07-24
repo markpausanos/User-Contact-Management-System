91cfe1921066e5464f3186764bc12b13609027fb
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-24 20:58:21 +0800

    Add credentials in UsersService

--- app/User Contact Management System/src/services/UsersService.js
+++ app/User Contact Management System/src/services/UsersService.js
@@ -5,10 +5,13 @@ const BASE_URL = `${config.API_URL}/Users`;
 const REFRESH_TOKEN_URL = `${BASE_URL}/Tokens/Refresh`;
 
 const UsersService = {
-	login: (user) => axios.post(`${BASE_URL}/Login`, user),
-	register: (user) => axios.post(`${BASE_URL}/Register`, user),
+	login: (user) =>
+		axios.post(`${BASE_URL}/Login`, user, { withCredentials: true }),
+	register: (user) =>
+		axios.post(`${BASE_URL}/Register`, user, { withCredentials: true }),
 	refreshToken: (userTokenRequest) =>
-		axios.post(REFRESH_TOKEN_URL, userTokenRequest),
+		axios.post(REFRESH_TOKEN_URL, userTokenRequest, { withCredentials: true }),
+	get: () => axios.get(BASE_URL, { withCredentials: true }),
 	updatePassword: (userUpdatePassword) =>
 		axios.put(`${BASE_URL}`, userUpdatePassword),
 	updateDetails: (userUpdateDetails) =>
