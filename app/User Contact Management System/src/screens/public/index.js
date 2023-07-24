b5ec2a4225b1a7a5350c3c932321b323b3be6b6a
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-24 20:59:23 +0800

    Add index.js for screens

--- app/User Contact Management System/src/screens/public/index.js
+++ app/User Contact Management System/src/screens/public/index.js
@@ -1,2 +1,5 @@
 export { default as Login } from "./Login";
+export { default as ForgotPassword } from "./ForgotPassword";
 export { default as ScreenNoExist } from "./ScreenNoExist";
+export { default as SignUp } from "./SignUp";
+export { default as Home } from "./Home";
