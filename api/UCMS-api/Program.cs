a50805aeaf23d66682c6094a7ed5ececf5226c81
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-07-26 10:02:26 +0800

    Add only dev swagger

--- api/UCMS-api/Program.cs
+++ api/UCMS-api/Program.cs
@@ -35,8 +35,12 @@ await ConfigureServices(builder.Services, builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+ 
 
 app.UseCors();
 app.UseHttpsRedirection();
