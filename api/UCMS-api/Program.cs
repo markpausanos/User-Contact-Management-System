973b9fbaf3afbd26e42be294adf0f3f230543527
Mark Vincent Pausanos <markvincent.pausanos@cit.edu>
2023-07-26 12:18:36 +0800

    Add docker support for API

--- api/UCMS-api/Program.cs
+++ api/UCMS-api/Program.cs
@@ -1,5 +1,3 @@
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using FoolProof.Core;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -31,14 +29,14 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-await ConfigureServices(builder.Services, builder.Configuration);
+ConfigureServices(builder.Services, builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "UCMS API"); });
 }
  
 
@@ -50,19 +48,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-
 app.Run();
 
-async Task ConfigureServices(IServiceCollection services, IConfiguration configuration)
+void ConfigureServices(IServiceCollection services, IConfiguration configuration)
 {
-    var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri") ?? builder.Configuration.GetSection("KeyVault:URL").Value!).ToString();
-    
-    var secretClient = new SecretClient(new Uri(keyVaultEndpoint!), new DefaultAzureCredential());
-
-    KeyVaultSecret jwtSecret = await secretClient.GetSecretAsync("jwt-secret");
-    KeyVaultSecret dbSecret = await secretClient.GetSecretAsync("azure-sqldb-connection");
-
-    string? dbSecretValue = dbSecret.Value ?? configuration.GetConnectionString("APIDbContext");
+    string? dbSecretValue = configuration.GetConnectionString("APIDbContext");
     services.AddDbContext<APIDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString(dbSecretValue!)));
 
@@ -106,7 +96,7 @@ async Task ConfigureServices(IServiceCollection services, IConfiguration configu
 
     services.Configure<JwtConfig>(configuration.GetSection("JwtConfig"));
     var jwtConfig = configuration.GetSection("JwtConfig").Get<JwtConfig>();
-    jwtConfig.Secret = jwtSecret.Value ?? jwtConfig.Secret;
+
     var tokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
